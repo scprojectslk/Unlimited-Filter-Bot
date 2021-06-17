d69e36e07e678084e2f4945df726d726faab8300
scprojectslk <80474245+scprojectslk@users.noreply.github.com>
2021-06-17 13:17:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("🍀Flix Movies LK🍀", url="flixmovieslk.xyz"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/SCProjectsLK"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/scprojectsLKdiscuss")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/SCProjectsLK")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
