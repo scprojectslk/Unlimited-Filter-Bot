8eb7d33597c0ee8db01cfc544d312f7416488f05
scprojectslk <80474245+scprojectslk@users.noreply.github.com>
2021-06-17 13:14:15 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("🍀Flix Movies LK🍀", url="flixmovieslk.xyz"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/SCProjectsLK"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/scprojectsLKdiscuss")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/SCProjectsLK")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
